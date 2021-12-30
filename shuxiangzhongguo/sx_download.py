41295eadba9be42cc1770c114192b542363c9a9e
chenping <1005978353@qq.com>
2021-12-30 20:25:30 +0800

    new version 193

--- shuxiangzhongguo/sx_download.py
+++ shuxiangzhongguo/sx_download.py
@@ -3,6 +3,7 @@ import time
 import random
 import os, sys
 import hashlib
+import pdfkit
 
 # linux usage:python downshuxiang.py http://shuxiang.chineseall.cn/v3/book/read/Mu4yg/pdf/ 326
 
@@ -11,7 +12,7 @@ import hashlib
 # cookies={'_Tvt5MJ89bV_':'605E52501D5E3259C8B628C3A95B7B3DB143DAAE10C929CE9CA9D88DB5991EA6FFCAF2B47CD0A00F1F9B6E34EFE097E19B14A0EE5D082FBE891C6FAE0253BFCC'}
 
 
-def login(url,totalpage):
+def login():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36', \
         'Host': 'shuxiang.chineseall.cn',
@@ -26,17 +27,7 @@ def login(url,totalpage):
     loginurl = 'http://shuxiang.chineseall.cn/sso/ajaxLogin.action'
 
     r = s.post(loginurl, data=data)
-    if len(sys.argv) >1:
-        bookurl = sys.argv[1]
-        totalpage = int(sys.argv[2])
-    else:
-        bookurl = url
-
-
-    bookurl = bookurl.replace('read', 'content')
-    bookurl = bookurl.replace('PDF', 'pdf')
-
-    return s,bookurl,totalpage
+    return s
 
 
 def progressbar(processnum,totalnum):
@@ -48,33 +39,70 @@ def progressbar(processnum,totalnum):
 
 
 if __name__ == '__main__':
+
     if os.name != "posix":
-        windows_url ='http://shuxiang.chineseall.cn/v3/book/read/Q8TJg/PDF/'
-        totalpage = 190
-    else:
-        windows_url = sys.argv[1]
+        book_url ='http://shuxiang.chineseall.cn/v3/book/read/Ng2Nj/EPUB/'
+        # book_url ='http://shuxiang.chineseall.cn/v3/book/read/Q8TJg/PDF/'
+        totalpage = 28
+    else:                        #linux 平台
+        book_url = sys.argv[1]
         totalpage = sys.argv[2]
 
-    s,bookurl,totalpage = login(windows_url,totalpage)
+    s = login()
+
+
     try:
+        bookurl = book_url.replace('read', 'content')
+
+        pdf = 0  #标志
+        file_path = "resource/"
+
+        if book_url.__contains__('PDF'):
+
+            bookurl = bookurl.replace('PDF', 'pdf')
+            file_suffix = ".jpg"
+            pdf = 1
+        else:
+            bookurl = bookurl.replace('EPUB', 'epub')
+            file_suffix= ".html"
+
         for i in range(1, totalpage + 1):
             delay = random.randint(1, 10) * 0.3
-
             time.sleep(delay)
 
             atime = time.time() * 1000
             requesttime = str(atime).split('.')[0]
-
             # rtext=requests.get(bookurl+str(i)+'?t='+str(requesttime),headers=headers,cookies=cookies)
             rtext = s.get(bookurl + str(i) + '?t=' + str(requesttime))
 
-            with open('resource/'+str(i) + '.jpg', 'wb') as f:
+
+            with open(file_path+str(i) + file_suffix, 'wb') as f:
+                if pdf == 0:
+                    f.write(b'<head><link href="epub/style_css/page_styles.css"  type="text/css"/></head>')
                 f.write(rtext.content)
 
             progressbar(i, totalpage)
     except Exception as e:
         print(e)
     finally:
+
+        #对 html 文件进行转换
+        config = pdfkit.configuration(wkhtmltopdf='D:\\Program Files (x86)\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+        options = {
+            'page-size': 'A4',
+            'margin-top': '0.8in',
+            'margin-right': '0.6in',
+            'margin-bottom': '0.6in',
+            'margin-left': '0.5in',
+            'encoding': "UTF-8",
+        }
+
+        html_file_list = os.listdir(file_path)
+        for each_file in html_file_list:
+            name,name_type = os.path.splitext(each_file)
+            pdfkit.from_file(configuration= config,options = options,
+                             input = file_path+each_file,output_path='epub/'+name+".pdf",
+                             css = 'epub/style_css/page_styles.css')
 
         print("")
         # logout
