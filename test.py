bb8badc7e296050728a7016c9f1c682e122a738a
chenping <1005978353@qq.com>
2022-04-21 16:35:45 +0800

    new version 199

--- test.py
+++ test.py
@@ -1,89 +1,155 @@
-import asyncio
-import time
-import requests
-from concurrent.futures import ThreadPoolExecutor
-from utils.db import connect_sqlserver
-
-
-
-def _http(url):
-    # arg = url.split('?')[1]
-    # time.sleep(len(arg))
-    # return arg
-    res = requests.get(url)
-    return res.text
-
-async def async_http():
-
-
-    loop = asyncio.new_event_loop()
-    executor = ThreadPoolExecutor(2)
-    tasks =[]
-    for custid in [12,123]:
-        url = 'http://10.255.255.31:8000/?a={}'.format(custid)
-        tasks.append(loop.run_in_executor(executor,_http,url))
-
-    return await asyncio.gather(*tasks)
-
-
-def async_main():
-    start = time.time()
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    loop.run_until_complete(async_http())
-    # loop.close()
-    end = time.time()
-    print('花费时间{}s'.format(end - start))
-
-
-def _db(cursor,email):
-    sql = "select custid,Email from Customers where Email like '%{}'".format(email)
-    # time.sleep(len(email))
-    # return len(email)
-    cursor.execute(sql)
-    return cursor.fetchall()
-
-
-
-def aync_db(executor):
-    _, cursor = connect_sqlserver('customer')
-    start = time.time()
-    loop = asyncio.get_event_loop()
-    # loop = asyncio.get_running_loop()
-    tasks = []
-    for email in ['22cn.com', '264.net']:
-        sql = "select custid,Email from Customers where Email like '%{}'".format(email)
-        # temp = async_db(loop, cursor,email)
-        # task = loop.create_task(temp)
-        task  = loop.run_in_executor(executor, _db,cursor, email)
-        tasks.append(task)
-
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
-    end = time.time()
-    for task in tasks:
-        print("task ret:", task.result())
-    print('花费时间{}s'.format(end - start))
-
-
-
-def sync_http():
-    start = time.time()
-
-    for custid in [12,134]:
-        # url = "http://10.255.255.99:8100/customer/loginapi/get_user_viptype.php?custid={}".format(custid)
-        url = 'http://10.255.255.31:8000/?a={}'.format(custid)
-        res = requests.get(url)
-        print("ret :"+res.text)
-    end = time.time()
-    print('花费时间{}s'.format(end - start))
+import os
+import sys
+from os import path
+import logging.config
+import json,math,random,datetime
+from utils.util import simplediff
+from clickhouse_driver import Client,connect
+from utils import db
+from utils.load import readini
+import pymysql
+from utils.load import readconfini
 
 
 
 
-# aync_db(executor)
-async_main()
-# sync_http()
+def sqldecorate(obj):
+    father_path = os.path.dirname(os.path.dirname(__file__))
+
+    filepath = os.path.join(father_path,"config","testenv.ini")
+    cf = readini.readini(filepath)
 
 
+    return obj
+def loadenv(**kwargs):
+    env = readconfini('./conf/')
+
+    def wrap_o(func):
+        def wrap_in(self,**args):       #如果函数带参数，使用实参
+
+            #host
+            if args.__contains__('host') and args['host'] is not None:
+                host=args['host']
+            else:
+                host = env.get(kwargs['db'], 'db_host')
+
+            # port
+            if args.__contains__('port') and args['port'] is not None:
+                port = args['port']
+            else:
+                port = int(env.get(kwargs['db'], 'db_port'))
+
+            #user
+            if args.__contains__('user') and args['user'] is not None:
+                user = args['user']
+            else:
+                user = env.get(kwargs['db'], 'db_username')
+
+            #password
+            if args.__contains__('password') and args['password'] is not None:
+                password = args['password']
+            else:
+                password = env.get(kwargs['db'], 'db_password')
+
+            #database
+            if args.__contains__('database') and args['database'] is not None:
+                database=args['database']
+            else:
+                database = env.get(kwargs['db'], 'db_name')
+
+            #collection
+            if args.__contains__('collection') and args['collection'] is not None:
+                collection=args['collection']
+            else:
+                if env.has_option(kwargs['db'],'db_collection'):
+                    collection = env.get(kwargs['db'],'db_collection')
+                else:
+                    collection = None
+            func(self,host,port,user,password,database,collection)
+        return wrap_in
+    return wrap_o
+    pass
+
+class PyDB():
+
+    def __init__(self):
+        self.conn = None
+        self.cusor = None
+        pass
+
+    def close_db(self):
+        """关闭数据库"""
+        if self.cursor is not None:
+            self.cursor.close()
+        if self.conn is not None:
+            self.conn.close()
+
+    def get_result_from_db(self,sql):
+
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return result
+
+
+
+
+def load(func):
+
+    def wrap(self,*args,**kw):
+        host = '10.7.30.148'
+        port = 9000
+        user = 'membersbi'
+        password = 'dangdangbi'
+        database = 'ioc'
+        func(self,host, port, user, password, database)
+    return wrap
+
+
+class PyCK(PyDB):
+
+    @loadenv(db='db_ck')
+    def __init__(self,host=None, port=None, user=None, password=None, database=None, collection=None):
+        self.conn = Client(host=host, port=port, user=user, password=password, database=database)
+        pass
+
+    def get_result_from_db(self, sql):
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return result
+        pass
+
+
+class PyMysql(PyDB):
+    @loadenv(db='db_mysql')
+    def __init__(self, host=None, port=None, user=None, password=None, database=None, collection=None):
+        self.conn = pymysql.connect(host=host, port=port, user=user, password=password, db=database)
+        self.cursor = self.conn.cursor()
+
+
+
+def test_ck():
+
+    ck_sql =" select * from dm_report.dm_reco_kpi_product  where product_id='22770934' " \
+         "and date_str='2022-04-20' and platform='3' and shop_type=1"
+
+    mysql_sql ="select * from project_info;"
+
+    # ck_db = PyCK()
+    # result = ck_db.get_result_from_db(ck_sql)
+
+
+    mysql_db = PyMysql(host = '10.255.255.31',port=3307,user ='root',password ='123456',database='project')
+
+    for i in range(1,4):
+        sql ="select * from project_info where id = {};".format(i)
+        result = mysql_db.get_result_from_db(sql)
+        yield result
+
+    mysql_db.close_db()
+
+
+
+
+a= test_ck()
+for ele in a:
+    print(ele)
